a8cc8559c3bf549445bd8ea6bd816d81dc25bd46
Aaron.Nacua <alnacua@gmail.com>
2018-03-20 14:31:51 -0500

    Completed all steps

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Search.css';
+import "./Search.css";
 
-import SearchIcon from 'react-icons/lib/md/search';
+import SearchIcon from "react-icons/lib/md/search";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+  handleChange(value) {
+    this.setState({ input: value });
+  }
   render() {
+    console.log(this.state.input);
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input
+            placeholder="Search Your Feed"
+            onChange={e => this.handleChange(e.target.value)}
+          />
 
           <SearchIcon id="Search__icon" />
         </div>
-        
       </section>
-    )
+    );
   }
 }
